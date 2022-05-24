4134e0672528d6e69354696061a339e4454069e1
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-05-24 07:58:30 +0200

    laby jeu

--- zeldiablo/src/JeuSimple/LabyJeu.java
+++ zeldiablo/src/JeuSimple/LabyJeu.java
@@ -1,9 +1,20 @@
 package JeuSimple;
 
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import moteurJeu.Clavier;
 import moteurJeu.Jeu;
 
 public class LabyJeu implements Jeu {
+
+    private Circle Personnage;
+    private Rectangle[] Murs;
+
+
+    public LabyJeu(Circle p, Rectangle[] m){
+        this.Murs = m;
+        this.Personnage = p;
+    }
 
 
     @Override
