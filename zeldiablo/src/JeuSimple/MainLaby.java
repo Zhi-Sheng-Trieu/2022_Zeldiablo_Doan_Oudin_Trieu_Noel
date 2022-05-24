33fa268d4916e69743a57009945660722a580b37
Alexandre Noel <noel295u@etu.univ-lorraine.fr>
2022-05-24 11:05:34 +0200

    MainLaby

---
+++ zeldiablo/src/JeuSimple/MainLaby.java
@@ -0,0 +1,14 @@
+package JeuSimple;
+
+import JeuSimple.LabyJeu;
+
+import java.io.IOException;
+
+public class MainLaby {
+
+    public static void main(String[] args) throws IOException {
+
+        LabyJeu labyJeu = new LabyJeu("labySimple/laby1.txt");
+
+    }
+}
