ccc4aa996542677641e4e61bdf3b87080a428931
Clement Oudin <clementoudin3@gmail.com>
2022-05-24 15:19:48 +0200

    monstre

--- zeldiablo/src/gameLaby/laby/Labyrinthe.java
+++ zeldiablo/src/gameLaby/laby/Labyrinthe.java
@@ -157,7 +157,7 @@ public class Labyrinthe {
         int[] suivante = getSuivant(courante[0], courante[1], action);
 
         // si c'est pas un mur ou un monstre, on effectue le deplacement
-        if (!this.murs[suivante[0]][suivante[1]] && this.monstre.getX() != suivante[0] && this.monstre.getY() != suivante[1]) {
+        if (!this.murs[suivante[0]][suivante[1]] && (this.monstre.getX() != suivante[0] && this.monstre.getY() != suivante[1])) {
             // on met a jour personnage
             this.pj.x = suivante[0];
             this.pj.y = suivante[1];
