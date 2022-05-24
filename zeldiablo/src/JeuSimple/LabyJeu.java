da1076d9e47b3ecc3620c28c088653138fa94b12
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-05-24 07:53:00 +0200

    labyjeu2

--- zeldiablo/src/JeuSimple/LabyJeu.java
+++ zeldiablo/src/JeuSimple/LabyJeu.java
@@ -1,8 +1,23 @@
 package JeuSimple;
 
+import moteurJeu.Clavier;
 import moteurJeu.Jeu;
 
 public class LabyJeu implements Jeu {
 
 
+    @Override
+    public void update(double secondes, Clavier clavier) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public boolean etreFini() {
+        return false;
+    }
 }
