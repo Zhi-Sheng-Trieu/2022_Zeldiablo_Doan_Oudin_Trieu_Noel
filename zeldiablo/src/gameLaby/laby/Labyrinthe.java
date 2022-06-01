a1cdd7101cf96a84e22af8a46140bfe4257da915
Alexandre Noel <noel295u@etu.univ-lorraine.fr>
2022-06-01 14:48:27 +0200

    Ajout diagramme de séquence

--- zeldiablo/src/gameLaby/laby/Labyrinthe.java
+++ zeldiablo/src/gameLaby/laby/Labyrinthe.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Stack;
 
 import static JeuSimple.LabyDessin.TAILLE;
 
@@ -303,7 +304,6 @@ public class Labyrinthe {
         }
         return valide;
     }
-
 
 
     /**
--- zeldiablo/src/gameLaby/laby/Main.java
+++ zeldiablo/src/gameLaby/laby/Main.java
@@ -1,6 +1,7 @@
 package gameLaby.laby;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * charge et affiche un labyrinthe
@@ -9,15 +10,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         // charge le labyrinthe
-        Labyrinthe laby = new Labyrinthe("zeldiablo/labySimple/labyPassageSecret1.txt");
+        Labyrinthe laby = new Labyrinthe("zeldiablo/labySimple/labyMonstre.txt");
 
         System.out.println(laby);
-        laby.deplacerPerso("Gauche");
-        System.out.println(laby);
-        laby.deplacerPerso("Haut");
-        System.out.println(laby);
-        laby.deplacerPerso("Gauche");
-        laby.deplacerPerso("Bas");
-        System.out.println(laby);
+        ArrayList<Position> positions = laby.recherche(laby.monstre.get(0).getPos());
+        System.out.println(positions);
     }
 }
